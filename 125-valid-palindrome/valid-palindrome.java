d2ff29a278b6073b7985dcd55b8c35757edbf4be
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-06-18 22:04:08 +0530

    Time: 3 ms (72.77%) | Memory: 43.3 MB (53.91%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int start  = 0;
+        int end = s.length()-1;
+        s = s.toLowerCase();
+        while(start<=end){
+            char ch1 = s.charAt(start);
+            char ch2 = s.charAt(end);
+            if(!(ch1>='a' && ch1<='z' ||ch1>='0' && ch1 <='9')){
+                start++;
+                continue;
+            }
+            if(!(ch2>='a' && ch2<='z' ||ch2>='0' && ch2 <='9')){
+                end--;
+                continue;
+            }
+            if(ch1!=ch2){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
