f84e18ea509b8d4d413db11e9072402c0282c493
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-09-12 18:01:30 +0530

    Time: 3 ms (80.32%) | Memory: 16.8 MB (28.22%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -12,7 +12,15 @@ public:
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
+        if(n<=1){
+            return 0;
+        }
         vector<int>dp(n+1,-1);
-        return helper(n,cost,dp);
+        dp[0]=0;
+        dp[1] = 0;
+        for(int i=2;i<=n;i++){
+            dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+        }
+        return dp[n];
     }
 };
