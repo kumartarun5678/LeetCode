16a62a2d517094f5e0fbf5d0a17a174d55f6962e
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-09-16 03:20:00 +0530

    Time: 100 ms (78.78%) | Memory: 15.5 MB (82.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -11,21 +11,21 @@ public:
         }
 
         int target = total / 2;
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-        for(int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
-        for(int i = 1; i <= n; i++) {
+        vector<int>curr(total+1,0);
+        vector<int>next(total+1,0);
+        curr[0] = 1;
+        next[0] = 1;
+        for(int i = 1; i <=n; i++) {
             for(int j = 1; j <= target; j++) {
                 bool take = false;
                 if(j - nums[i-1] >= 0) {
-                    take = dp[i-1][j - nums[i-1]];
+                    take = next[j - nums[i-1]];
                 }
-                bool ntake = dp[i-1][j];
-                dp[i][j] = take || ntake;
+                bool ntake = next[j];
+                curr[j] = take || ntake;
             }
+            next = curr;
         }
-        return dp[n][target];
+        return next[target];
     }
 };
