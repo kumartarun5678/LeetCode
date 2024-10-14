20e6372059f3b15156727271258f0f75c92bf9b1
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-10-15 01:42:16 +0530

    Time: 183 ms (85.35%) | Memory: 75.9 MB (87.88%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    static long long maxKelements(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(), nums.end());
-        long long score=0;
-        for(int i=0; i<k; i++){
-            int x=pq.top();
-            score+=x;
-            if (x==1){// early stop
-                score+=(k-1-i);
-                break;
-            }
+    long long maxKelements(vector<int>& nums, int k) {
+        long long sum = 0;
+        priority_queue<int>pq(nums.begin(),nums.end());
+
+        while(k--){
+            int maxe = pq.top();
             pq.pop();
-            pq.push((x+2)/3);
+
+            sum += maxe;
+
+            maxe = ceil(maxe/3.0);
+            pq.push(maxe);
         }
-        return score;
+        return sum;
     }
 };
-
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
