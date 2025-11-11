2b6510a82038b5ea36e05d797a276ff931ccb777
Tarun Kumar <kumartarun5678@gmail.com>
2025-11-12 01:31:41 +0530

    Time: 5 ms (84.49%) | Memory: 56.8 MB (69.57%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.ts
@@ -0,0 +1,21 @@
+function isIsomorphic(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+
+    const mapST: Record<string, string> = {};
+    const mapTS: Record<string, string> = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        if ((mapST[charS] && mapST[charS] !== charT) ||
+            (mapTS[charT] && mapTS[charT] !== charS)) {
+            return false;
+        }
+
+        mapST[charS] = charT;
+        mapTS[charT] = charS;
+    }
+
+    return true;
+}
